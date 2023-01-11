a1a8f1dda30d0d3d2fae35baac3230c471cff0cc
Aymane Jeddad <mmvn02@gmail.com>
2023-01-11 20:02:17 +0100

    Commited

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Form from './Components/form';
+// import App from './Components/Boxes/App';
+// import AppJoke from './Components/Jokes/AppJoke';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
