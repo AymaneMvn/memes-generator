41d885cea2c87bfe18d09b34a9dae9238a7c0217
AYMANE JEDDAD <mmvn02@gmail.com>
2023-02-10 19:41:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Form from './Components/form';
-// import App from './Components/Boxes/App';
-// import AppJoke from './Components/Jokes/AppJoke';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
